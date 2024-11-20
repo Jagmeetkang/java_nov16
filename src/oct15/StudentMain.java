9cc884b5a0c383a4aa8f736e6e4a70b9fe2330a1
Jagmeetkang <Jagmeetkang01@gmail.com>
2024-11-20 13:01:33 -0500

    toString()

---
+++ src/oct15/StudentMain.java
@@ -0,0 +1,8 @@
+package oct15;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student = new Student("101","Armaan",5000.78);
+        System.out.println(student);
+    }
+}
